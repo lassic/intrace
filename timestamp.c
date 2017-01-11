84c74501e5e7c9cf7929aabc0715055bc6340c17
Daniel Davidovich <daniel.davidovich@forter.com>
2017-01-11 11:04:06 +0200

    time is in mili

--- timestamp.c
+++ timestamp.c
@@ -6,5 +6,5 @@
 uint64_t get_timestamp() {
     struct timespec cur;
     clock_gettime(CLOCK_MONOTONIC, &cur);
-    return (cur.tv_sec) * 1000000 + (cur.tv_nsec) / 1000;
+    return (cur.tv_sec) * 1000 + (cur.tv_nsec) / 1000000;
 }
